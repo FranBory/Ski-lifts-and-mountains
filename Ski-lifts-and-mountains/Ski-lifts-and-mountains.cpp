4423ab8ffa9930dfcd8bbf4b8f5f9b8910041bc9
Franciszek Borys <franekborys2003@gmail.com>
2025-04-26 09:41:10 +0200

    first commit

--- Ski-lifts-and-mountains/Ski-lifts-and-mountains.cpp
+++ Ski-lifts-and-mountains/Ski-lifts-and-mountains.cpp
@@ -1,20 +1,76 @@
-﻿// Ski-lifts-and-mountains.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+#define MAX_WIDTH  2001   
+#define MAX_HEIGHT 2001   
 
-int main()
-{
-    std::cout << "Hello World!\n";
+typedef struct Lift {
+	int end_row;
+	int end_col;
+	int cost;
+	int time;
+	struct Lift* next_lift;
+} Lift;
+
+
+typedef struct {
+	int height;
+	int lift_count;
+	Lift* lifts;
+} Node;
+
+
+void addLiftToNode(Node* node,Lift* lift) {
+	if (node->lift_count == 0) {
+		lift->next_lift = nullptr;
+		node->lifts = lift;
+	}
+	else {
+		lift->next_lift = node->lifts;
+		node->lifts = lift;
+	}
+	node->lift_count++;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+Node map[MAX_HEIGHT][MAX_WIDTH];
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+int main() {
+
+	for (int r = 0; r < MAX_HEIGHT; ++r) {
+		for (int c = 0; c < MAX_WIDTH; ++c) {
+			map[r][c].lift_count = 0;
+		}
+	}
+
+	int width, height;
+	int start_col, start_row;
+	int end_col, end_row;
+	int lift_numb;
+	//TODO: wyciagi
+	scanf_s("%d %d", &width, &height);
+	scanf_s("%d %d", &start_col, &start_row);
+	scanf_s("%d %d", &end_col, &end_row);
+	scanf_s("%d", &lift_numb);
+
+	for (int i = 0; i < lift_numb; i++) {
+		int time,cost;
+		scanf_s("%d %d", &start_col, &start_row);
+		scanf_s("%d %d", &end_col, &end_row);
+		scanf_s("%d %d", &time, &cost);
+		Lift* lift = (Lift*)malloc(sizeof(Lift));
+		addLiftToNode(&map[start_col][start_row], lift);
+	}
+
+	for (int r = 0; r < height; ++r) {
+		for (int c = 0; c < width; ++c) {
+			int input;
+			scanf_s("%d", &input);
+			map[r][c].height = input;
+		}
+	}
+
+	printf("0");
+
+	return 0;
+}
+
